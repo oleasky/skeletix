48b6044e36138ed29a529d94afa4290f8f0228e8
Andrei Olea <andrei@andreiolea.com>
2025-03-05 17:43:16 -0500

    🔧 fix(Button.js): fix padding value in link button styles 🔊 chore(detectBarestyles.js): update console.warn message for missing TailwindCSS plugin "barestyles"

--- dist/components/Button/Button.js
+++ dist/components/Button/Button.js
@@ -10,7 +10,7 @@ const Button = (props) => {
     const { type, children, className = '' } = props;
     if (type === 'link') {
         const { href, newTab } = props; // Extract specific props for link
-        return (react_1.default.createElement("a", { href: href, target: newTab ? '_blank' : undefined, rel: newTab ? 'noopener noreferrer' : undefined, className: `px-4 py-2 text-[#FFFFFF] bg-[#64748b] rounded hover:bg-[#0f172a] ${compId} ${className}` },
+        return (react_1.default.createElement("a", { href: href, target: newTab ? '_blank' : undefined, rel: newTab ? 'noopener noreferrer' : undefined, className: `p-4 text-[#FFFFFF] bg-[#64748b] rounded hover:bg-[#0f172a] ${compId} ${className}` },
             react_1.default.createElement("span", { className: `${compId}__text` }, children)));
     }
     return (react_1.default.createElement("button", { onClick: props.onClick, className: `px-4 py-2 text-[#FFFFFF] bg-[#64748b] rounded hover:bg-[#0f172a] ${compId} ${className}` },
